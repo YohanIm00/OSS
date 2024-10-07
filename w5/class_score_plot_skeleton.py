e34ac0c7083685f1238ac088ea53486240b77ffd
Yohan Im <119671375+YohanIm00@users.noreply.github.com>
2024-10-07 16:38:33 +0900

    feat: complete Scatter Plot #7
    - Complete the part of displaying scatter plot

--- w5/class_score_plot_skeleton.py
+++ w5/class_score_plot_skeleton.py
@@ -13,14 +13,26 @@ if __name__ == '__main__':
     class_kr = read_data('data/class_score_kr.csv')
     class_en = read_data('data/class_score_en.csv')
 
-    # TODO) Prepare midterm, final, and total scores
+    # Prepare midterm, final, and total scores
     midterm_kr, final_kr = zip(*class_kr)
     total_kr = [40/125*midterm + 60/100*final for (midterm, final) in class_kr]
     midterm_en, final_en = zip(*class_en)
     total_en = [40/125*midterm + 60/100*final for (midterm, final) in class_en]
 
-    # TODO) Plot midterm/final scores as points
-
+    # Plot midterm/final scores as points
+    # Set axes' range
+    
+    
+    # Make both scatter plot
+    plt.scatter(midterm_kr, final_kr, c='red', marker='.')
+    plt.scatter(midterm_en, final_en, c='blue', marker='+')
+    # Modify scatter plot's trivial features
+    plt.legend(labels=['Korean', 'English'], loc= "upper left")
+    plt.xlabel('Midterm scores')
+    plt.xlim(0, 125)
+    plt.ylabel('Final scores')
+    plt.ylim(0, 100)
+    plt.grid()
 
     # TODO) Plot total scores as a histogram
 
